053c56e0463cb9f492f51b7dcc5de31977874555
chengwen-zheng <chengwenzheng1222@gmail.com>
2024-05-28 08:48:24 +0800

    refactor: move the frame type to own file

---
+++ src/resp/map.rs
@@ -0,0 +1,81 @@
+use std::{
+    collections::BTreeMap,
+    ops::{Deref, DerefMut},
+};
+
+use bytes::{Buf, BytesMut};
+
+use crate::{RespDecode, RespEncode, RespError, RespFrame, SimpleString};
+
+use super::{calc_total_length, parse_length, BUF_CAP, CRLF_LEN};
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub struct RespMap(BTreeMap<String, RespFrame>);
+
+impl RespDecode for RespMap {
+    const PREFIX: &'static str = "%";
+
+    fn decode(buf: &mut BytesMut) -> Result<Self, RespError> {
+        let (end, len) = parse_length(buf, Self::PREFIX)?;
+        let total_len = calc_total_length(buf, end, len, Self::PREFIX)?;
+
+        if buf.len() < total_len {
+            return Err(RespError::NotComplete);
+        }
+
+        buf.advance(end + CRLF_LEN);
+
+        let mut frames = RespMap::new();
+        for _ in 0..len {
+            let key = SimpleString::decode(buf)?;
+            let value = RespFrame::decode(buf)?;
+            frames.insert(key.0, value);
+        }
+
+        Ok(frames)
+    }
+
+    fn expect_length(buf: &[u8]) -> Result<usize, RespError> {
+        let (end, len) = parse_length(buf, Self::PREFIX)?;
+        calc_total_length(buf, end, len, Self::PREFIX)
+    }
+}
+
+// - map: "%<number-of-entries>\r\n<key-1><value-1>...<key-n><value-n>"
+// we only support string key which encode to SimpleString
+impl RespEncode for RespMap {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(BUF_CAP);
+        buf.extend_from_slice(&format!("%{}\r\n", self.len()).into_bytes());
+        for (key, value) in self.0 {
+            buf.extend_from_slice(&SimpleString::new(key).encode());
+            buf.extend_from_slice(&value.encode());
+        }
+        buf
+    }
+}
+impl Deref for RespMap {
+    type Target = BTreeMap<String, RespFrame>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for RespMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl RespMap {
+    pub fn new() -> Self {
+        RespMap(BTreeMap::new())
+    }
+}
+
+impl Default for RespMap {
+    fn default() -> Self {
+        RespMap::new()
+    }
+}
